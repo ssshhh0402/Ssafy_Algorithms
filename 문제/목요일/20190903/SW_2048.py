f700a07c1c83f8d50541d762b81e5d95ab22f58c
soonbum <ssshhh0402@naver.com>
2019-09-05 23:36:09 +0900

    20190905|2014

--- 문제/목요일/20190903/SW_2048.py
+++ 문제/목요일/20190903/SW_2048.py
@@ -9,14 +9,52 @@ def change(a,b,c):
         if not 0 <= x < int(N) or not 0 <= y < int(N) or base[a][b] == 0:
             break
         elif base[x][y] == 0:
-            if x == 0 or x == int(N) - 1:
-                imsi = base[a][b]
-                base[a][b] = 0
-                base[x][y] = imsi
-            else:
-                stck.append((x, y))
-
-        elif  base[x][y] == base[a][b] and not visited[x][y]:
+            if c == 2 or c == 3:
+                if x == 0 or x == int(N) - 1:
+                    while 0 <= y <= int(N)-1:
+                        if base[x][y] == base[x-dx[c]][y-dy[c]] and not visited[x][y]:
+                            base[x][y] += base[x-dx[c]][y-dy[c]]
+                            base[x-dx[c]][y-dy[c]] = 0
+                            visited[x][y] = 1
+                            break
+                        elif base[x][y] != 0:
+                            break
+                        else:
+                            imsi = base[x - dx[c]][y - dy[c]]
+                            base[x - dx[c]][y - dy[c]] = 0
+                            base[x][y] = imsi
+                            x, y = x+dx[c], y+dy[c]
+                elif y == 0 or y == int(N) - 1:
+                    n = stck.pop(0)
+                    base[x][y] = base[n[0]][n[1]]
+                    base[n[0]][n[1]] = 0
+                # imsi = base[a][b]
+                # base[a][b] = 0
+                # base[x][y] = imsi
+                else:
+                    stck.append((x, y))
+            elif c == 0 or c == 1:
+                if y == 0 or y == int(N) -1:
+                    while 0 <= x <= int(N)-1 :
+                        if base[x][y] == base[x - dx[c]][y - dy[c]] and not visited[x][y]:
+                            base[x][y] += base[x - dx[c]][y - dy[c]]
+                            base[x - dx[c]][y - dy[c]] = 0
+                            visited[x][y] = 1
+                            break
+                        elif base[x][y] !=0:
+                            break
+                        else:
+                            imsi = base[x - dx[c]][y - dy[c]]
+                            base[x - dx[c]][y - dy[c]] = 0
+                            base[x][y] = imsi
+                            x, y = x+dx[c], y+dy[c]
+                elif x == 0 or x == int(N) - 1:
+                    n = stck.pop(0)
+                    base[x][y] = base[n[0]][n[1]]
+                    base[n[0]][n[1]] = 0
+                else:
+                    stck.append((x,y))
+        elif base[x][y] == base[a][b] and not visited[x][y]:
             base[x][y] += base[a][b]
             base[a][b] = 0
             visited[x][y] = 1
