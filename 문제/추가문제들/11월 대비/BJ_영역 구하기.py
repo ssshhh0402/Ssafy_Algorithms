6b1f59558721723151ce9e9cf0e4ba70f58935f4
soonbum <ssshhh0402@naver.com>
2019-10-04 22:21:55 +0900

    20191004|회장 이동

--- 문제/추가문제들/11월 대비/BJ_영역 구하기.py
+++ 문제/추가문제들/11월 대비/BJ_영역 구하기.py
@@ -18,18 +18,21 @@ def find(a,b):
 
 M,N,K = map(int,input().split())
 visited = [[0 for _ in range(N)] for _ in range(M)]
-cant = []
 id = 0
 result = []
 for _ in range(K):
-    arr = list(map(int,input().split()))
-    for i in range(M-arr[0]-1, M - arr[2]-1, -1):
-        for j in range(N - arr[1]-1, N - arr[3]-2, -1):
-            visited[i][j] = 1
+    arr = list(map(int, input().split()))
+    for x in range(M-arr[3], M-arr[1]):
+        for y in range(arr[0], arr[2]):
+            visited[x][y] = 1
+
 
 for i in range(M):
     for j in range(N):
         if not visited[i][j]:
             id += 1
             visited[i][j] = 1
+            find(i,j)
 
+print(id)
+print(*sorted(result))
